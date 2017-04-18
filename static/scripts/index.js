68a2270685f6679afc96c56d3f757756e3ee7e97
ravenusmc <mcuddy77@gmail.com>
2017-04-17 20:18:08 -0400

    added more styling to login page

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,1 +1,8 @@
-
+//This function ensures that the user has entered the correct value at the login form.
+function validateForm(){
+    if (document.forms[0].username.value == "" || document.forms[0].password.value == "" ){
+        alert('Please ensure you enter in both a username or password');
+        return false;
+    }
+    return true;
+}
