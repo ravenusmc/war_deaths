6d410021477641616d40954a096809f4fa7ed393
ravenusmc <mcuddy77@gmail.com>
2017-04-30 11:03:46 -0400

    minor change

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
 
-# set the secret key.  keep this really secret:
+# set the secret key. keep this really secret:
 app.secret_key = 'n3A\xef(\xb0Cf^\xda\xf7\x97\xb1x\x8e\x94\xd5r\xe0\x11\x88\x1b\xb9'
 
 #This line will actually run the app.
